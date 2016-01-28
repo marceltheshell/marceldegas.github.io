e4bd04fdb7ff993f03ecdf540a289ce684224cc3
Marcel Degas <marcel.degas@gmail.com>
2016-01-28 09:22:07 -0800

    scaled imaged to fit background

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,15 @@
-$(function(){
-  $(".element").typed({
-    strings: ["Marcel Degas.", "a Software Developer.",  "an Educator.", "A student of Life.", "a Writer.", "a Family Guy.", "a Minimalist.", "Focused on Here and Now."],
-    typeSpeed: 100,
-    startDelay: 100, 
-    loop: true,
-    backDelay: 500,
-    showCursor: true,    	
-  });
+$(document).ready(function() {
+	$(function(){
+	  $(".element").typed({
+	    strings: ["Marcel Degas.", "a Software Developer.",  "an Educator.", "a student of Life.", "a Writer.", "a Family Guy.", "a Minimalist.", "Focused on Here and Now."],
+	    typeSpeed: 100,
+	    startDelay: 100, 
+	    loop: true,
+	    backDelay: 500,
+	    showCursor: true,    	
+	  });
+	});
 });
-
 
 
 
