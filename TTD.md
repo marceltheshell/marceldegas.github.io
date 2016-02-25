8440f3f4dbe754865962e19d04d75f58342fb377
Marcel Degas <marcel.degas@gmail.com>
2016-02-24 23:03:15 -0800

    added some jquery to icons and words

--- TTD.md
+++ TTD.md
@@ -1,34 +1,15 @@
 
-Basic
-fix the picture on middle of page
-Jquery words no longer pushing down the rest of the paragraph
-icons on the cards
-
-
-
-
-
-Next Steps
-Twitter box
-
-
-Tae's steps
-remove bootstrap
-make two row divs (display inline block or table)
-try fixing the image to one place 
-try padding on col 2
-fixed height to the div with the jquery 50px 
-relative col-1 and absolute the icons
--do absolute for the rest of percentages??
-
-Challenge
--get name to work 
+Challenge 
 -give a more sleek filter over the whole thing
 -do jquery animation hover-over something in phone
+-Safari mobile is not working
+
+Maybe
+-change background to a wheat field
 
 
 media queries
--@media queries for md, sm & xs
+-@media queries xs
 -do for nexus6 
--do for ipad
+
 
--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,36 @@ $(document).ready(function() {
 	    showCursor: false,    	
 	  });
 	});
+	$(".git-icon").hover(function(){
+	  $('.phone-git-icon').show();
+		}, function() {
+	  		$('.phone-git-icon').hide();
+	});
+	$(".email-icon").hover(function(){
+	  $('.phone-email-icon').show();
+		}, function() {
+	  		$('.phone-email-icon').hide();
+	});
+	$(".linked-icon").hover(function(){
+	  $('.phone-linked-icon').show();
+		}, function() {
+	  		$('.phone-linked-icon').hide();
+	});
+	$(".medium-icon").hover(function(){
+	  $('.phone-medium-icon').show();
+		}, function() {
+	  		$('.phone-medium-icon').hide();
+	});
+	$(".medium-word").hover(function(){
+	  $('.phone-medium-icon').show();
+		}, function() {
+	  		$('.phone-medium-icon').hide();
+	});
+	$(".klassroom-word").hover(function(){
+	  $('.phone-medium-icon').show();
+		}, function() {
+	  		$('.phone-medium-icon').hide();
+	});
 });
 
 
