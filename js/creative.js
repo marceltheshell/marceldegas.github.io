e38304a6a16823e4e892017ba83829b4311401c6
Marcel Degas <marcel.degas@gmail.com>
2016-07-10 22:09:43 -0700

    finished the jquery slider for now

--- js/creative.js
+++ js/creative.js
@@ -1,55 +1,4 @@
 $('document').ready(function() {
-    // var speed = 500;            // Fade speed
-    // var autoswitch = true;      // Autoslider options
-    // var autoswitch_speed = 4000; // Autoslider speed
-
-    // //Add initial active class
-    // $('.slide').first().addClass('active');
-
-    // //Hide all slides
-    // $('.slide').hide();
-
-    // //Show active slide
-    // $('.active').show();
-
-    // // Switch to next slide
-    // $('#next').on('click', nextSlide)
-
-    // // Switch to previous slide
-    // $('#prev').on('click', previousSlide)
-
-    // //Autoslider
-    // if (autoswitch === true) {
-    //     setInterval(nextSlide, autoswitch_speed);
-    // }
-
-    // function nextSlide() {
-    //     $('.active').removeClass('active').addClass('previousActive');
-    //     if( $('.previousActive').is(':last-child') ) {
-    //         $('.slide').first().addClass('active');
-    //     }
-    //     else {
-    //         $('.previousActive').next().addClass('active');
-    //     }
-    //     $('.previousActive').removeClass('previousActive');
-    //     $('.slide').fadeOut(speed);
-    //     $('.active').fadeIn(speed);
-    // }
-
-    // function previousSlide() {
-    //     $('.active').removeClass('active').addClass('previousActive');
-    //     if( $('.previousActive').is(':first-child') ) {
-    //         $('.slide').last().addClass('active');
-    //     }
-    //     else {
-    //         $('.previousActive').prev().addClass('active');
-    //     }
-    //     $('.previousActive').removeClass('previousActive');
-    //     $('.slide').fadeOut(speed);
-    //     $('.active').fadeIn(speed);
-    // }
-
-
     (function($) {
         "use strict"; // Start of use strict
 
@@ -93,11 +42,12 @@ $('document').ready(function() {
 
         $(function(){
           $(".element").typed({
-            strings: ["Marcel Degas.", "a Software Developer.", "a French, Spanish and Arabic Speaker.", "a Family Guy.", "a California Native.", "ENFJ.", "a Smelly Cheese Lover."],
+            strings: ["Hello there", "I am Marcel Degas", "Welcome."],
             typeSpeed: 50,
-            loop: true,
-            backDelay: 500,
-            showCursor: false,      
+            startDelay: 1000,
+            loop: false,
+            backDelay: 2000,
+            showCursor: true,      
           });
         });
 
---
+++ js/mySlider.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function($) {
+
+    function moveLeft() {
+        $('#slider ul').animate({
+            left: + slideWidth
+        }, 200, function () {
+            $('#slider ul li:last-child').prependTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    function moveRight() {
+        $('#slider ul').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $('#slider ul li:first-child').appendTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    $('a.control_prev').click(function () {
+        moveLeft();
+    });
+
+    $('a.control_next').click(function () {
+        moveRight();
+    });
+})
